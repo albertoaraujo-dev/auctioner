c0c5eaf663e7528463184b18cb38020f0bae56f3
Alberto Araujo <albertoaraujo.dev@gmail.com>
2024-03-11 16:33:31 -0300

    Initial config for tests.

---
+++ tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -0,0 +1,19 @@
+﻿using auction.API.UseCases.Auctions.GetCurrent;
+using Xunit;
+
+namespace UseCases.Test.Auctions.GetCurrent;
+public class GetCurrentAuctionUseCaseTest
+{
+    [Fact]
+    public void Success()
+    {
+
+        //ARRANGE
+        var useCase = new GetCurrentAuctionUseCase();
+
+        //ACT
+
+        //ASSERT
+
+    }
+}
